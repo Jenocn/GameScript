ad64bc1f3396b647452149afb8fd243a5bd7a264
Jenocn <544673717@qq.com>
2020-03-19 11:01:34 +0800

    Logger代码优化

--- GameScript/src/Logger.cs
+++ GameScript/src/Logger.cs
@@ -1,39 +1,38 @@
 ﻿/*
  * By Jenocn
  * https://jenocn.github.io/
-*/
+ */
 
 using System;
 
 namespace gs.compiler {
 	public static class Logger {
 
-		private static Action<string> _func = null;
+		private static Action<string> _logFunc = null;
+
+		static Logger() {
+			SetLoggerFunc(null);
+		}
 
 		public static void SetLoggerFunc(Action<string> func) {
-			_func = func;
+			if (_logFunc != null) {
+				_logFunc = func;
+			} else {
+				_logFunc = (string message) => {
+					Console.WriteLine(message);
+				};
+			}
 		}
 
 		public static void Log(string message) {
-			if (_func != null) {
-				_func(message);
-			} else {
-				Console.WriteLine(message);
-			}
+			_logFunc(message);
 		}
 
 		public static void Error(string src, string message = "") {
-			if (_func != null) {
-				_func(" ");
-				_func(string.Format("> Error:{0}", message));
-				_func(src);
-				_func("--------------------------------");
-			} else {
-				Console.WriteLine(" ");
-				Console.WriteLine("> Error:{0}", message);
-				Console.WriteLine(src);
-				Console.WriteLine("--------------------------------");
-			}
+			_logFunc(" ");
+			_logFunc(string.Format("> Error:{0}", message));
+			_logFunc(src);
+			_logFunc("--------------------------------");
 		}
 	}
 }
